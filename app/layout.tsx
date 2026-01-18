2d3ba3f2dc6e8d37738413daa6875f63b1e71d9c
tintin4303 <nyunt.tin4303@gmail.com>
2026-01-19 00:49:38 +0700

    add Social Bar ads script in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import Script from "next/script"; 
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <Script 
+          src="https://pl28511143.effectivegatecpm.com/ae/bb/42/aebb42672f361b1bbaace491b5e03bc3.js" 
+          strategy="afterInteractive" 
+        />
+        </Providers>
       </body>
     </html>
   );
